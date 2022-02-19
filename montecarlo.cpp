6b3f86577a54e88a53a2e9442747e4e2eb7d9724
kusano <kusano@users.noreply.github.com>
2022-02-19 16:29:18 +0900

    1人ずつ処理
    10575000

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -25,16 +25,18 @@ struct Field
     int N, M;
     vector<int> px, py, pt;
     vector<int> hx, hy;
+    vector<int> hx_old, hy_old;
 
     // 0: 空き, 1: 通行不能
     vector<vector<int>> F;
     vector<vector<int>> P;
     vector<vector<int>> H;
     int turn;
+    int turn_sub;
 
     Field(vector<int> px, vector<int> py, vector<int> pt, vector<int> hx, vector<int> hy)
         : N(int(px.size()))
-        , M(int(py.size()))
+        , M(int(hx.size()))
         , px(px)
         , py(py)
         , pt(pt)
@@ -44,135 +46,153 @@ struct Field
         , P(S, vector<int>(S))
         , H(S, vector<int>(S))
         , turn(0)
+        , turn_sub(0)
     {
+        hx_old.resize(M);
+        hy_old.resize(M);
+
         for (int i=0; i<N; i++)
             P[py[i]][px[i]]++;
         for (int i=0; i<M; i++)
             H[hy[i]][hx[i]]++;
     }
 
-    vector<string> get_random_pet_moves() const
+    string get_random_pet_move() const
     {
-        vector<string> ret(N);
+        if (!(M<=turn_sub))
+            throw "not pet turn";
 
-        for (int i=0; i<N; i++)
+        int p = turn_sub-M;
+        int n;
+        switch (pt[p])
         {
-            int n;
-            switch (pt[i])
-            {
-            case 0: n = 1;
-            case 1: n = 2;
-            case 2: n = 3;
-            case 3: n = 2;
-            case 4: n = 2;
-            }
-
-            string m;
-            int x = px[i];
-            int y = py[i];
-            for (int j=0; j<n; j++)
-            {
-                vector<int> D;
-                for (int d=0; d<4; d++)
-                {
-                    int tx = x + dir_x[d];
-                    int ty = y + dir_y[d];
-                    if (0<=tx && tx<S && 0<=ty && ty<S && F[ty][tx]==0)
-                        D.push_back(d);
-                }
-                int d = D[xor64()%(int)D.size()];
-                x += dir_x[d];
-                y += dir_y[d];
-                m += dir_move[d];
-            }
-            ret[i] = m;
+        case 0: n = 1;
+        case 1: n = 2;
+        case 2: n = 3;
+        case 3: n = 2;
+        case 4: n = 2;
         }
-        return ret;
+
+        string move;
+        int x = px[p];
+        int y = py[p];
+        for (int j=0; j<n; j++)
+        {
+            vector<int> D;
+            for (int d=0; d<4; d++)
+            {
+                int tx = x + dir_x[d];
+                int ty = y + dir_y[d];
+                if (0<=tx && tx<S && 0<=ty && ty<S && F[ty][tx]==0)
+                    D.push_back(d);
+            }
+            int d = D[xor64()%(int)D.size()];
+            x += dir_x[d];
+            y += dir_y[d];
+            move += dir_move[d];
+        }
+        return move;
     }
 
-    //  各人の可能な動きを返す
-    vector<string> get_human_moves() const
+    //  人の可能な動きを返す
+    string get_human_moves() const
     {
-        vector<string> moves;
-        for (int i=0; i<M; i++)
+        if (!(turn_sub<M))
+            throw "not human turn";
+
+        int h = turn_sub;
+        string moves = ".";
+        for (int d=0; d<4; d++)
         {
-            string m = ".";
-            for (int d=0; d<4; d++)
+            int x = hx[h]+dir_x[d];
+            int y = hy[h]+dir_y[d];
+            if (0<=x && x<S && 0<=y && y<S &&
+                F[y][x]==0 && P[y][x]==0 && H[y][x]==0)
             {
-                int x = hx[i]+dir_x[d];
-                int y = hy[i]+dir_y[d];
-                if (0<=x && x<S && 0<=y && y<S &&
-                    F[y][x]==0 && P[y][x]==0 && H[y][x]==0)
+                bool ok = true;
+                for (int d2=0; d2<4 && ok; d2++)
                 {
-                    bool ok = true;
-                    for (int d2=0; d2<4 && ok; d2++)
-                    {
-                        int x2 = x+dir_x[d2];
-                        int y2 = y+dir_y[d2];
-                        if (0<=x2 && x2<S && 0<=y2 && y2<S &&
-                            // とりあえず人に隣接しているところも不可
-                            (P[y2][x2]>0 || (H[y2][x2]>0 && (x2!=hx[i]||y2!=hy[i])) || (H[y2][x2]>1 && (x2==hx[i]&&y2==hy[i])) ))
-                            ok = false;
-                    }
-                    if (ok)
-                        m += dir_obst[d];
+                    int x2 = x+dir_x[d2];
+                    int y2 = y+dir_y[d2];
+                    if (0<=x2 && x2<S && 0<=y2 && y2<S &&
+                        P[y2][x2]>0)
+                        ok = false;
                 }
+                if (ok)
+                    moves += dir_obst[d];
             }
-            for (int d=0; d<4; d++)
-            {
-                int x = hx[i]+dir_x[d];
-                int y = hy[i]+dir_y[d];
-                if (0<=x && x<S && 0<=y && y<S &&
-                    F[y][x]==0)
-                    m += dir_move[d];
-            }
-            moves.push_back(m);
         }
+        for (int d=0; d<4; d++)
+        {
+            int x = hx[h]+dir_x[d];
+            int y = hy[h]+dir_y[d];
+            if (0<=x && x<S && 0<=y && y<S &&
+                F[y][x]==0)
+                moves += dir_move[d];
+        }
+
         return moves;
     }
 
-    void move(string humans, vector<string> pets)
+    void move(string move)
     {
-        for (int i=0; i<N; i++)
-            for (int j=0; j<(int)pets[i].size(); j++)
-                for (int d=0; d<4; d++)
-                    if (pets[i][j]==dir_move[d])
-                    {
-                        int x = px[i];
-                        int y = py[i];
-                        P[y][x]--;
-                        x += dir_x[d];
-                        y += dir_y[d];
-                        P[y][x]++;
-                        px[i] = x;
-                        py[i] = y;
-                    }
-
-        for (int i=0; i<M; i++)
+        if (turn_sub<M)
         {
+            int h = turn_sub;
+            hx_old[h] = -1;
             for (int d=0; d<4; d++)
-                if (humans[i]==dir_obst[d])
+                if (move[0]==dir_obst[d])
                 {
-                    int x = hx[i];
-                    int y = hy[i];
+                    int x = hx[h];
+                    int y = hy[h];
                     x += dir_x[d];
                     y += dir_y[d];
                     F[y][x] = 1;
                 }
             for (int d=0; d<4; d++)
-                if (humans[i]==dir_move[d])
+                if (move[0]==dir_move[d])
                 {
-                    int x = hx[i];
-                    int y = hy[i];
-                    H[y][x]--;
+                    int x = hx[h];
+                    int y = hy[h];
+                    hx_old[h] = x;
+                    hy_old[h] = y;
+                    // 元いた場所も通行不可にはできないから後で
+                    //H[y][x]--;
                     x += dir_x[d];
                     y += dir_y[d];
                     H[y][x]++;
-                    hx[i] = x;
-                    hy[i] = y;
+                    hx[h] = x;
+                    hy[h] = y;
                 }
         }
-        turn++;
+        else
+        {
+            int p = turn_sub-M;
+            for (char m: move)
+                for (int d=0; d<4; d++)
+                    if (m==dir_move[d])
+                    {
+                        int x = px[p];
+                        int y = py[p];
+                        P[y][x]--;
+                        x += dir_x[d];
+                        y += dir_y[d];
+                        P[y][x]++;
+                        px[p] = x;
+                        py[p] = y;
+                    }
+
+        }
+        turn_sub++;
+        if (turn_sub>=N+M)
+        {
+            for (int i=0; i<M; i++)
+                if (hx_old[i]>=0)
+                    H[hy_old[i]][hx_old[i]]--;
+
+            turn_sub = 0;
+            turn++;
+        }
     }
 };
 
@@ -190,7 +210,7 @@ int main()
     }
     int M;
     cin>>M;
-    vector<int> hx(N), hy(N);
+    vector<int> hx(M), hy(M);
     for (int i=0; i<M; i++)
     {
         cin>>hx[i]>>hy[i];
@@ -201,16 +221,22 @@ int main()
     Field field(px, py, pt, hx, hy);
     for (int t=0; t<T; t++)
     {
-        vector<string> moves = field.get_human_moves();
-        string human;
+        string ans;
         for (int i=0; i<M; i++)
-            human += moves[i][xor64()%(int)moves[i].size()];
-        cout<<human<<endl;
+        {
+            string moves = field.get_human_moves();
+            string move = moves.substr(xor64()%(int)moves.size(), 1);
 
-        vector<string> pets(N);
-        for (string &move: pets)
+            ans += move;
+            field.move(move);
+        }
+        cout<<ans<<endl;
+
+        for (int i=0; i<N; i++)
+        {
+            string move;
             cin>>move;
-
-        field.move(human, pets);
+            field.move(move);
+        }
     }
 }
---
+++ test.sh
@@ -0,0 +1,14 @@
+set -eu
+
+g++ -O2 montecarlo.cpp
+
+pushd tools
+
+for i in $(seq 0 99)
+do
+  name=$(printf %04d.txt ${i})
+  echo ${name} 1>&2
+  cargo run --release --bin tester ../a.out < in/${name} > out/${name}
+done
+
+popd
